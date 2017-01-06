1b915cbb14d8c068ee537ec9f39144830ac40cfe
lame <grejnom@gmail.com>
2017-01-06 22:30:37 +0100

    foobar

--- FramesThreadsTest.java
+++ FramesThreadsTest.java
@@ -5,6 +5,9 @@
  */
 package frames.threads.test;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 /**
  *
  * @author dedged
@@ -15,7 +18,14 @@ public class FramesThreadsTest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+         JFrame frame = new JFrame("Hello, world!");
+         frame.setSize(250, 300);
+         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         JPanel panel1 = new JPanel();
+         frame.add(panel1);
+         
+         panel1.setLayout(null);
+         
     }
     
 }
