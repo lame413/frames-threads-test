ffbe664cf27c6f356aabed7342ab20f98bf87d2f
lame <grejnom@gmail.com>
2017-01-07 20:28:36 +0100

    made some frames and moved them around!

--- FramesThreadsTest.java
+++ FramesThreadsTest.java
@@ -5,8 +5,11 @@
  */
 package frames.threads.test;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import java.awt.Color;
+import java.util.HashSet;
+import javax.swing.*;
+//import javax.swing.JFrame;
+//import javax.swing.JPanel;
 
 /**
  *
@@ -17,15 +20,60 @@ public class FramesThreadsTest {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-         JFrame frame = new JFrame("Hello, world!");
-         frame.setSize(250, 300);
-         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-         JPanel panel1 = new JPanel();
-         frame.add(panel1);
-         
-         panel1.setLayout(null);
-         
+    public static void main(String[] args) throws InterruptedException {
+        JFrame frame = new JFrame("Hello, world!");
+        frame.setSize(250, 250);
+        frame.setVisible(true);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setBackground(Color.gray);
+
+        JPanel bfbg = new JPanel();
+        bfbg.setBackground(Color.gray);
+        bfbg.setLayout(null);
+        bfbg.setVisible(true);
+
+        JPanel boxBlack = new JPanel();
+        boxBlack.setSize(50, 50);
+        boxBlack.setLayout(null);
+        boxBlack.setBackground(Color.black);
+        boxBlack.setLocation(127, 0);
+        boxBlack.setVisible(true);
+
+        JPanel boxWhite = new JPanel();
+        boxWhite.setSize(50, 50);
+        boxWhite.setLayout(null);
+        boxWhite.setBackground(Color.white);
+        boxWhite.setLocation(0, 127);
+        boxWhite.setVisible(true);
+
+        frame.add(bfbg);
+        bfbg.add(boxBlack);
+        bfbg.add(boxWhite);
+        //frame.add(boxBlack);
+        //frame.add(boxWhite);
+        frame.setSize(250, 250);
+
+        frame.setLocation(250, 250);
+        frame.repaint();
+
+        try {
+            Thread.sleep(1000);//1sec
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+
+        boxWhite.setLocation(200, 127);
+        boxBlack.setLocation(127, 200);
+        frame.repaint();
+
+        try {
+            Thread.sleep(1000);//1sec
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+        
+        frame.dispose();
     }
-    
+
 }
